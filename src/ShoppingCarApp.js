d4009702e8da75324da13a9bc6a1c9fbe798a70b
Cristian Daniel Morales Villada <cristian980829@gmail.com>
2021-11-07 16:46:32 -0500

    PROJECT CREATED

---
+++ src/ShoppingCarApp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const ShoppingCarApp = () => {
+    return (
+        <div className="container mt-3">
+            <h1>Shopping Car App</h1>
+        </div>
+    )
+}
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ShoppingCarApp } from './ShoppingCarApp';
+
+ReactDOM.render(
+    <ShoppingCarApp />,
+  document.getElementById('root')
+);
