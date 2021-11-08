cd4517f1c53f0c6dc5de619baabf6b518144a3fa
Cristian Daniel Morales Villada <cristian980829@gmail.com>
2021-11-08 14:38:56 -0500

    PRODUCTS LIST

--- src/components/products/ProductItem.js
+++ src/components/products/ProductItem.js
@@ -6,6 +6,7 @@ export const ProductItem = ({data}) => {
             <div className="card-body">
                 <h5 className="card-title">{data.id}</h5>
                 <p className="card-text">{data.name}</p>
+                <span className="card-text">${data.price}.00</span>
             </div>
         </div>
     )
--- src/components/products/ProductsScreen.js
+++ src/components/products/ProductsScreen.js
@@ -1,12 +1,12 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { ProductItem } from './ProductItem';
 
 export const ProductsScreen = () => {
 
-    const products = [{
-        name:"leche",
-        id:123
-    },{name:"gaseosa",id:1256}]
+    const { products } = useSelector( state => state.shoppingCar );
+
+    console.log(products);
 
     return (
         <>
